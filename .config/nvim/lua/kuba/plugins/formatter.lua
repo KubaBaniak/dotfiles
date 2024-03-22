25df5909fe6160c914ec15d69684432b9fc9b60d
Jakub Urbaniak <kuba121201@gmail.com>
2024-03-22 09:01:10 +0100

    chore: update formatter and neogit setup

--- .config/nvim/lua/kuba/plugins/formatter.lua
+++ .config/nvim/lua/kuba/plugins/formatter.lua
@@ -19,5 +19,14 @@ return {
         }
       }
     }
+
+    local augroup = vim.api.nvim_create_augroup
+    local autocmd = vim.api.nvim_create_autocmd
+
+    augroup("__formatter__", { clear = true })
+    autocmd("BufWritePost", {
+      group = "__formatter__",
+      command = ":FormatWrite",
+    })
   end
 }
--- .config/nvim/lua/kuba/plugins/neogit.lua
+++ .config/nvim/lua/kuba/plugins/neogit.lua
@@ -1,13 +1,13 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",  -- required
+    "nvim-lua/plenary.nvim", -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed, not both.
     "nvim-telescope/telescope.nvim", -- optional
     --"ibhagwan/fzf-lua",              -- optional
   },
-
-    vim.api.nvim_set_keymap("n", "<Leader>g", [[:Neogit kind=vsplit<CR>]], { noremap = true, silent = true })
+  config = true,
+  vim.api.nvim_set_keymap("n", "<Leader>g", [[:Neogit kind=vsplit<CR>]], { noremap = true, silent = true }),
 }
