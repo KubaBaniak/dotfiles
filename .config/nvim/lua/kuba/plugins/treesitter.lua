655a119944a14fbc51f06151e97283c86d14bd7a
kuba <kuba121201@gmail.com>
2026-03-28 10:10:22 +0100

    Add yaml parser for codecompanion

--- .config/nvim/lua/kuba/plugins/treesitter.lua
+++ .config/nvim/lua/kuba/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "html" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "typescript", "javascript", "tsx", "html", "yaml" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
