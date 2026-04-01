3bc7b597ee45ca9be2fc23f1d4e11f828cc1069b
Jakub Urbaniak <jakub.urbaniak@all-for-one.com>
2026-04-01 15:15:02 +0200

    use codecompanion mcp setup

--- .config/nvim/lua/kuba/plugins/ai/codecompanion.lua
+++ .config/nvim/lua/kuba/plugins/ai/codecompanion.lua
@@ -7,10 +7,19 @@ return {
     "nvim-telescope/telescope.nvim",
     "stevearc/dressing.nvim",
     "MeanderingProgrammer/render-markdown.nvim",
-    "ravitemer/mcphub.nvim",
   },
   config = function()
     require("codecompanion").setup({
+      mcp = {
+        servers = {
+          ["sequential-thinking"] = {
+            cmd = { "npx", "-y", "@modelcontextprotocol/server-sequential-thinking" },
+          },
+          ["obsidian"] = {
+            cmd = { "npx", "-y", "@bitbonsai/mcpvault@latest", "/home/kuba41/obsidian_vault" },
+          },
+        },
+      },
       prompt_library = {
         markdown = {
           dirs = {
@@ -31,23 +40,6 @@ return {
         },
         inline = { adapter = { name = "copilot", model = "claude-sonnet-4.6" } },
         agent = { adapter = { name = "copilot", model = "claude-sonnet-4.6" } },
-      },
-      extensions = {
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            -- MCP Tools
-            make_tools = true, -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
-            show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
-            add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
-            show_result_in_chat = true, -- Show tool results directly in chat buffer
-            format_tool = nil, -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
-            -- MCP Resources
-            make_vars = false, -- Convert MCP resources to #variables for prompts
-            -- MCP Prompts
-            make_slash_commands = true, -- Add MCP prompts as /slash commands
-          },
-        },
       },
       opts = {
         log_level = "ERROR",
